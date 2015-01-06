6ab7e97138be6b025a1db3bfe547b603356b1492
Arthur <arthur.peka@outlook.com>
2015-01-06 15:09:18 +0200

    Adding unique username constraint + limiting email length

--- Raptorex.DA/Configurations/UserConfig.cs
+++ Raptorex.DA/Configurations/UserConfig.cs
@@ -1,6 +1,8 @@
 ﻿using Raptorex.BO.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Infrastructure.Annotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,18 @@ namespace Raptorex.DA.Configurations
 {
     public class UserConfig : BaseConfig<RaptorexUser>
     {
-        public UserConfig() : base("Users") { }
+        public UserConfig() : base("Users") 
+        {
+            Property(u => u.Username)
+                .IsRequired()
+                .HasMaxLength(512)
+                .HasColumnAnnotation(IndexAnnotation.AnnotationName,
+                    new IndexAnnotation(new IndexAttribute("IX_Username") { IsUnique = true }));
+
+            //Yes, I know it should be 254 (as IETF says)!
+            //However, originally it was 320, and there certainly are non-compliant addresses, so let's keep it 320
+            Property(u => u.Email)
+                .HasMaxLength(320); 
+        }
     }
 }
