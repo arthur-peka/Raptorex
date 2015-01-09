8f5708b307fb94fba71bba0356c792d3970d3f90
Arthur <arthur.peka@outlook.com>
2015-01-09 17:22:21 +0200

    Adding NOT NULL constraint to Subforum's Title column + specifying max length of it

--- Raptorex.BO/Entities/Subforum.cs
+++ Raptorex.BO/Entities/Subforum.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace Raptorex.BO.Entities
     /// </summary>
     public class Subforum : BaseEntity
     {
+        [Required]
+        [MaxLength(3000)]
         public string Title { get; set; }
 
 
