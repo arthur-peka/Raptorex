b63b706f5604d11d428b9642121f6d9765a5308a
Arthur <arthur.peka@outlook.com>
2015-01-11 01:32:56 +0200

    Adding integration test project (for DA layer)

---
+++ Raptorex.DA.Tests/UserRepositoryTests.cs
@@ -0,0 +1,37 @@
+﻿using Raptorex.BO.Entities;
+using Raptorex.DA.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Raptorex.DA.Tests
+{
+    public class UserRepositoryTests
+    {
+        public UserRepositoryTests()
+        {
+            using (var context = new RaptorexContext())
+            {
+                context.Database.Delete();
+                context.Database.Initialize(force: false);
+            }
+        }
+
+        [Fact]
+        public void TestUserAddition()
+        {
+            UserRepository ur = new UserRepository();
+            string newUsersUsername = "ivan2000";
+            var userToAdd = new RaptorexUser() { Email = "ivan@vasja.com", Username = newUsersUsername };
+
+            ur.Insert(userToAdd);
+
+            var userFromDb = ur.GetSingle(u => u.Username == newUsersUsername);
+
+            Assert.True(userFromDb != null);
+        }
+    }
+}
