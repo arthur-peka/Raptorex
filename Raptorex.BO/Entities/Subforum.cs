69a2cdf93c42f2ad69ae4ec0e4d3787d8f1f0479
Arthur <arthur.peka@outlook.com>
2015-01-13 21:52:10 +0200

    Subforum titles should be unique

--- Raptorex.BO/Entities/Subforum.cs
+++ Raptorex.BO/Entities/Subforum.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace Raptorex.BO.Entities
     {
         [Required]
         [MaxLength(3000)]
+        [Index("IX_SubforumTitle", IsUnique=true)]
         public string Title { get; set; }
 
 
