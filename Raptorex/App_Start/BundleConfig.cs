1b5e4816a1ad3b94292fdd6d138bd437d1485818
Arthur <arthur.peka@outlook.com>
2015-01-14 14:31:47 +0200

    Adding KO mapping plugin

--- Raptorex/App_Start/BundleConfig.cs
+++ Raptorex/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace Raptorex
                       "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
-                      "~/Scripts/knockout-3.2.0.js"));
+                      "~/Scripts/knockout-3.2.0.js",
+                      "~/Scripts/knockout.mapping-latest.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
