2f300a99aa19bae25f63cd68a679293b8afddfd1
Arthur <arthur.peka@outlook.com>
2015-01-14 15:11:48 +0200

    Adding necessary methods to forum threads controller + adding Subforum repository

---
+++ Raptorex.DA/Repositories/SubforumRepository.cs
@@ -0,0 +1,13 @@
+﻿using Raptorex.BO.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raptorex.DA.Repositories
+{
+    public class SubforumRepository : BaseRepository<Subforum>
+    {
+    }
+}
--- Raptorex/Controllers/api/ForumThreadsController.cs
+++ Raptorex/Controllers/api/ForumThreadsController.cs
@@ -3,31 +3,53 @@ using Raptorex.DA.Repositories;
 using Raptorex.Filters;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Web.Http;
+using System.Web.Security;
 
 namespace Raptorex.Controllers.api
 {
-    [RoutePrefix("api/threads")]
+    [RoutePrefix("api/forum")]
     public class ForumThreadsController : ApiController
     {
         private IBaseRepository<ForumTopic> _topicRepository;
+        private IBaseRepository<Subforum> _subforumRepository;
 
         public ForumThreadsController()
         {
             _topicRepository = new ForumTopicRepository();
+            _subforumRepository = new SubforumRepository();
         }
 
         [HttpGet]
-        [FormsAuthenticated]
         [Route("all")]
         public List<ForumTopic> GetAll()
         {
             var res = _topicRepository.Filter(x => true).ToList();
             var user = User.Identity.Name;
             return res;
+        }
+
+        [HttpGet]
+        [Route("subforums")]
+        public List<Subforum> GetAllSubforums()
+        {
+            var subforums = _subforumRepository.Filter(s => true).ToList();
+
+            return subforums;
+        }
+
+        [HttpGet]
+        [Route("subforum/{subforumId}")]
+        public List<ForumTopic> GetBySubforum(Guid subforumId)
+        {
+            var topics = _topicRepository.Filter(t => t.TopicSubforumId == subforumId).ToList();
+
+            return topics;
         }
     }
 }
